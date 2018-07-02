2820e2266a83f9a8933bbc58ab0f8cc08b3a1967
Daniel Wood <s.d.wood.82@googlemail.com>
2018-07-02 21:18:10 +0100

    Impliment help search
    fuzzy search based on fuse.js

---
+++ js/help/help.js
@@ -0,0 +1,15 @@
+var helpData = [
+{
+        "Title" : "Line",
+        "Type" : "Command",
+        "Shortcut" : "L",
+		"Description" : "Draws a line made from two point"
+    },
+     {
+        "Title" : "Circle",
+        "Type" : "Command",
+        "Shortcut" : "C",
+		"Description" : "Draws a Circle made from two point"
+    }
+
+]
