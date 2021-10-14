3775f5d09eb50749e3b4c374a651f8a33dc58d28
Daniel Wood <daniel.wood@aveva.com>
2021-10-14 19:44:10 +0100

    add type to point entity

--- js/entities/point.js
+++ js/entities/point.js
@@ -1,4 +1,5 @@
 function Point(x, y) {
+    this.type = "Point";
     this.x = 0;
     this.y = 0;
     if (x !== undefined) {
@@ -7,52 +8,52 @@ function Point(x, y) {
     }
 }
 
-Point.prototype.add = function (that) {
+Point.prototype.add = function(that) {
     return new Point(this.x + that.x, this.y + that.y);
 }
 
-Point.prototype.subtract = function (that) {
+Point.prototype.subtract = function(that) {
     return new Point(this.x - that.x, this.y - that.y);
 };
 
-Point.prototype.angle = function (that) {
+Point.prototype.angle = function(that) {
     //Angle between points in radians
     return Math.atan2((this.y - that.y), (this.x - that.x)) + Math.PI;
 }
 
-Point.prototype.clone = function () {
+Point.prototype.clone = function() {
     return new Point(this.x, this.y);
 };
 
-Point.prototype.distance = function (that) {
+Point.prototype.distance = function(that) {
     return Math.sqrt((this.x - that.x) * (this.x - that.x) + (this.y - that.y) * (this.y - that.y))
 }
 
-Point.prototype.dot = function (that) {
+Point.prototype.dot = function(that) {
     return this.x * that.x + this.y * that.y;
 };
 
-Point.prototype.rotate = function (centre, angle) {
+Point.prototype.rotate = function(centre, angle) {
     var x = centre.x + (this.x - centre.x) * Math.cos(angle) - (this.y - centre.y) * Math.sin(angle);
     var y = centre.y + (this.x - centre.x) * Math.sin(angle) + (this.y - centre.y) * Math.cos(angle);
     return new Point(x, y)
 }
 
-Point.prototype.min = function (that) {
+Point.prototype.min = function(that) {
     return new Point(
         Math.min(this.x, that.x),
         Math.min(this.y, that.y)
     );
 };
 
-Point.prototype.max = function (that) {
+Point.prototype.max = function(that) {
     return new Point(
         Math.max(this.x, that.x),
         Math.max(this.y, that.y)
     );
 };
 
-Point.prototype.midPoint = function (that) {
+Point.prototype.midPoint = function(that) {
     // return point midway between this and that
     var midX = (this.x + that.x) / 2
     var midY = (this.y + that.y) / 2
@@ -62,21 +63,21 @@ Point.prototype.midPoint = function (that) {
     return midPoint;
 }
 
-Point.prototype.lerp = function (that, t) {
+Point.prototype.lerp = function(that, t) {
     return new Point(
         this.x + (that.x - this.x) * t,
         this.y + (that.y - this.y) * t
     );
 };
 
-Point.fromPoints = function (p1, p2) {
+Point.fromPoints = function(p1, p2) {
     return new Point(
         p2.x - p1.x,
         p2.y - p1.y
     );
 };
 
-Point.prototype.project = function (angle, distance) {
+Point.prototype.project = function(angle, distance) {
     // project point from this along angle(radians) by distance
     x = this.x + Math.sin(angle) * distance
     y = this.y + Math.cos(angle) * distance
@@ -84,7 +85,7 @@ Point.prototype.project = function (angle, distance) {
     return p
 };
 
-Point.prototype.perpendicular = function (Pt1, Pt2) {
+Point.prototype.perpendicular = function(Pt1, Pt2) {
     //find the closest point on the straight line between Pt1 and Pt2
 
     var APx = this.x - Pt1.x;
