1305fd84c4d997905b33581a006ef8a74844863d
Daniel Wood <s.d.wood.82@googlemail.com>
2026-04-19 10:53:06 +0100

    Add canvas tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,5 @@
 import '@testing-library/jest-dom';
+import 'jest-canvas-mock';
+
+// jsdom does not provide ResizeObserver
+global.ResizeObserver = class { observe() {} unobserve() {} disconnect() {} };
